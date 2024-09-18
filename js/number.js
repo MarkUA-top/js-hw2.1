1c96f5e16a8129842355dca64d1880f46dc2d216
MarkUA <mark.d.nahorniak@gmail.com>
2024-09-18 18:18:29 +0300

    fix

---
+++ js/number.js
@@ -0,0 +1,41 @@
+//цельсії в фаренгейти
+// let C = 1
+// //ввести цифру яку ми хочем отримати в F
+// C = 10
+// let F = C * 1.8 + 32
+// console.log(F)
+
+//------------------
+
+// // фаренгейти в цельсії
+// let F = 1
+// //ввести цифру яку ми хочем отримати в С
+// F = 50
+// let C = (F - 32) / 1.8
+// console.log(C)
+
+//---------------------------------------------
+
+// //дні в місяці
+// let dayInMonth = 30
+// console.log(dayInMonth)
+// //годин в місяці
+// dayInMonth *= 24
+// console.log(dayInMonth)
+// //хвилин в місяці
+// dayInMonth *= 60
+// console.log(dayInMonth)
+
+//---------------------------------------------
+
+// let health = 10
+// health -= 3
+// let energy = 10
+// energy -= 3
+// console.log(health, energy)
+
+//---------------------------------------------
+
+// let cart = 100
+// cart = cart / 10 * 9
+// console.log(cart)
