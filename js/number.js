5b6b138041766ded533cbfa1f3bb07aec7bddfee
MarkUA-top <126385729+MarkUA-top@users.noreply.github.com>
2024-09-18 18:20:41 +0300

    Update number.js

--- js/number.js
+++ js/number.js
@@ -1,13 +1,13 @@
 //цельсії в фаренгейти
-// let C = 1
+let C = 1
 // //ввести цифру яку ми хочем отримати в F
-// C = 10
-// let F = C * 1.8 + 32
-// console.log(F)
+C = 10
+let F = C * 1.8 + 32
+console.log(F)
 
 //------------------
 
-// // фаренгейти в цельсії
+// фаренгейти в цельсії
 // let F = 1
 // //ввести цифру яку ми хочем отримати в С
 // F = 50
@@ -16,26 +16,26 @@
 
 //---------------------------------------------
 
-// //дні в місяці
-// let dayInMonth = 30
-// console.log(dayInMonth)
+// дні в місяці
+let dayInMonth = 30
+console.log(dayInMonth)
 // //годин в місяці
-// dayInMonth *= 24
-// console.log(dayInMonth)
+dayInMonth *= 24
+console.log(dayInMonth)
 // //хвилин в місяці
-// dayInMonth *= 60
-// console.log(dayInMonth)
+dayInMonth *= 60
+console.log(dayInMonth)
 
 //---------------------------------------------
 
-// let health = 10
-// health -= 3
-// let energy = 10
-// energy -= 3
-// console.log(health, energy)
+let health = 10
+health -= 3
+let energy = 10
+energy -= 3
+console.log(health, energy)
 
 //---------------------------------------------
 
-// let cart = 100
-// cart = cart / 10 * 9
-// console.log(cart)
+let cart = 100
+cart = cart / 10 * 9
+console.log(cart)
