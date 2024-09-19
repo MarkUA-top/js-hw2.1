437a1b9156ae340db9b4ee4fb9bfe348c86d6df9
MarkUA <mark.d.nahorniak@gmail.com>
2024-09-19 21:15:32 +0300

    fix

--- js/number.js
+++ js/number.js
@@ -1,9 +1,9 @@
 //цельсії в фаренгейти
-let c = 1
+// let c = 1
 // //ввести цифру яку ми хочем отримати в F
-c = 10
-let f = c * 1.8 + 32
-console.log(f)
+// c = 10
+// let f = c * 1.8 + 32
+// console.log(f)
 //------------------
 
 // фаренгейти в цельсії
@@ -12,33 +12,55 @@ console.log(f)
 // f = 50
 // let c = (f - 32) / 1.8
 // console.log(c)
-console.log("кінець")
+// console.log("кінець")
 
 //---------------------------------------------
 
-// дні в місяці
-let dayInMonth = 30
-console.log(dayInMonth)
-// //годин в місяці
-dayInMonth *= 24
-console.log(dayInMonth)
-// //хвилин в місяці
-dayInMonth *= 60
-console.log(dayInMonth)
-console.log("кінець")
+// // дні в місяці
+// let dayInMonth = 30
+// console.log(dayInMonth)
+// // годин в місяці
+// dayInMonth *= 24
+// console.log(dayInMonth)
+// // хвилин в місяці
+// dayInMonth *= 60
+// console.log(dayInMonth)
+// console.log("кінець")
 
 //---------------------------------------------
 
-let health = 10
-health -= 3
-let energy = 10
-energy -= 3
-console.log(health, energy)
-console.log("кінець")
+// let health = 10
+// health -= 3
+// let energy = 10
+// energy -= 3
+// console.log(health, energy)
+// console.log("кінець")
 
 //---------------------------------------------
 
-let cart = 100
-cart = cart / 10 * 9
-console.log(cart)
-console.log("кінець")
+// let cart = 100
+// cart = cart / 10 * 9
+// console.log(cart)
+// console.log("кінець")
+
+//---------------------------------------------
+
+// Друга частина дз
+
+// const number = "22.2px"
+// const numberFloat = Number.parseFloat(number)
+// console.log(numberFloat)
+
+//---------------------------------------------
+
+// const number = "22px"
+// const numberInt = Number.parseInt(number)
+// console.log(numberInt)
+
+//---------------------------------------------
+
+const number = "22px"
+const numberInt = Number.parseInt(number)
+console.log(numberInt)
+const numberString = Number.toString(numberInt)
+console.log(numberString)
